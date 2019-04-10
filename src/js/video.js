449f2ef230816a1112ce14144b62ab887b9f7255
Ben Keith <ben@benlk.com>
2019-04-09 22:01:55 -0400

    Add anotations to the page

--- src/js/video.js
+++ src/js/video.js
@@ -1,3 +1,5 @@
+// thanks to https://github.com/dinony/videojs-youtube-issue0/blob/master/src/main.js for getting this to work
+// got there from https://github.com/videojs/videojs-youtube/issues/473
 const videojs = require("video.js");
 require("videojs-overlay");
 require("videojs-youtube");
@@ -10,5 +12,9 @@ const player = videojs( 'video', {
 });
 player.src({
   "type": "video/youtube",
-  "src": document.getElementById('video').dataset.youtube
+  "src": document.getElementById('video').dataset.youtube,
+  "youtube": {
+    "rel": "0",
+    "modestbranding": "1"
+  }
 });
